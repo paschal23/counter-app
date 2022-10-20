f2598cb0f868ea307042c822b208dd751a47dd8b
paschal23 <116175627+paschal23@users.noreply.github.com>
2022-10-20 08:02:28 -0700

    add all basketball score counter

---
+++ var.js
@@ -0,0 +1,33 @@
+let count1 = 0
+let count2 = 0
+let Num1 = document.getElementById('num1')
+let Num2 = document.getElementById('num2')
+
+function add1() {
+    count1 += 1
+    Num1.textContent=count1
+}
+
+function add2(){
+    count1 += 2
+    Num1.textContent=count1
+
+}
+function add3(){
+    count1 += 3
+    Num1.textContent=count1
+}
+
+function add4() {
+    count2 += 1
+    Num2.textContent=count2
+}
+
+function add5(){
+    count2 += 2
+    Num2.textContent=count2
+}
+function add6(){
+    count2 += 3
+    Num2.textContent=count2
+}
