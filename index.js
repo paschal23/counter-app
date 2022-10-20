9619de7f62ad687ca0d5685f495a56959b696078
paschal23 <116175627+paschal23@users.noreply.github.com>
2022-10-20 10:40:09 -0700

    Add files via upload
    login details

---
+++ index.js
@@ -0,0 +1,9 @@
+let  Name= prompt(' Please enter your Name');
+let   occupa= prompt( Name + "," +'What do you do for a living' )
+let Age= prompt("Enter your age")
+let message1 = "your name is "
+let message4 =" you are "+occupa
+let message2 =" And you are "
+let message3=" year old "
+let  log = message1+Name+message4+message2+Age+message3
+alert(log)
